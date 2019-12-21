b0e4fc211684f5d188d2d68e8821827a77429ac0
Gorgchap <gorgchap@gmail.com>
2019-12-21 22:39:42 +0300

    New indents

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,10 @@
 import React from "react";
+import HomePage from "../HomePage/HomePage";
 
 function About() {
-    return (
-        <h2 className='text-center'>About</h2>
-    );
+  return <>
+    <h2 className='text-center'>About</h2>
+    <a href={<HomePage/>}>Link to home page</a>
+  </>;
 }
 export default About;
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import About from "../About/About";
 
 function HomePage() {
-    return <>
-        <h2 className='text-center'>Home Page</h2>
-        <a ></a>
-    </>;
+  return <>
+    <h2 className='text-center'>Home Page</h2>
+    <a href={<About/>}>Link to about page</a>
+  </>;
 }
 export default HomePage;
